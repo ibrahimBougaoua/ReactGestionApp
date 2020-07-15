c83e68e3589103ee026e31394fe936662bae0c89
ibrahimBougaoua <ibrahim.bougaoua@etu.usthb.dz>
2020-07-15 06:14:53 -0700

    Refactoring : model confirmation delete account user

--- src/components/user.js
+++ src/components/user.js
@@ -262,8 +262,33 @@ return (<div className="container mt-5">
                         </div>
                         <div className="form-group row mb-0">
                             <div className="col-md-6">
-                            <button type="button" className="btn btn-outline-danger" onClick={delete_user}>Delete</button>
+                            <button type="button" className="btn btn-outline-danger" data-toggle="modal" data-target="#exampleModalCenter">Delete this account</button>
                             </div>
+
+<div className="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div className="modal-dialog modal-dialog-centered" role="document">
+    <div className="modal-content">
+      <div className="modal-header">
+        <h5 className="modal-title" id="exampleModalCenterTitle">Delete this account</h5>
+        <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div className="modal-body">
+      Are you sure ? you'r account will deleted and you can't login anymore to this account ! 
+      </div>
+      <div className="modal-footer">
+        <button type="button" className="btn btn-outline-danger" onClick={delete_user}>Delete</button>
+        <button type="button" className="btn btn-outline-info" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+
+
                         </div>
                     </form>
                 </div>
