dba2f538d044789c7cb1d77134b044ec9caa5f54
ibrahimBougaoua <ibrahim.bougaoua@etu.usthb.dz>
2020-07-15 01:53:18 -0700

    add show comment and some funtions

---
+++ src/components/comment.js
@@ -0,0 +1,283 @@
+import React, { Component } from "react";
+import axios from 'axios';
+import Nav from './nav';
+
+// handle button click of login form
+async function getIntervention(id) {
+    try {
+      const response = await axios.get('http://127.0.0.1:8000/api/auth/intervention/' + id);
+      //console.log(response);
+      return response;
+    } catch (error) {
+      console.error(error);
+    }
+}
+
+async function all_signalisations() {
+    try {
+      const response = await axios.get('http://127.0.0.1:8000/api/auth/signalisation')
+      //console.log(response);
+      return response;
+    } catch (error) {
+      console.error(error);
+    }
+}
+
+async function all_chef() {
+  try {
+    const response = await axios.get('http://127.0.0.1:8000/api/auth/user')
+    //console.log(response);
+    return response;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function getChefByID(id) {
+    try {
+      const response = await axios.get('http://127.0.0.1:8000/api/auth/user/' + id)
+      //console.log(response);
+      return response;
+    } catch (error) {
+      console.error(error);
+    }
+}
+
+async function evaluer(intervention_id) {
+    try {
+      const response = await axios.get('http://127.0.0.1:8000/api/auth/ifevaluer/' + intervention_id)
+      console.log(response);
+      return response;
+    } catch (error) {
+      console.error(error);
+    }
+}
+
+export default class Comment extends Component {
+
+constructor(props) {
+    super(props);    
+    this.state = {signalisation_id: '',price: '',etat_avancement: '',date_debut: '',date_fin: '',name: '',chef_id: '',dataIntervention: [],select_signalisation: [],all: [],evaluers: [],chefs_ids: [],allChef: [],hasEvaluer: false};
+    
+    this.handleChangeSignalisation_id = this.handleChangeSignalisation_id.bind(this);
+    this.handleChangePrice = this.handleChangePrice.bind(this);
+    this.handleChangeEtatAvancement = this.handleChangeEtatAvancement.bind(this);
+    this.handleChangeDateDebut = this.handleChangeDateDebut.bind(this);
+    this.handleChangeDateFin = this.handleChangeDateFin.bind(this);
+    this.handleChangeChefId = this.handleChangeChefId.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChangeSignalisation_id(event) {
+    this.setState({signalisation_id: event.target.options[event.target.selectedIndex].value});
+  }
+
+  handleChangePrice(event) {
+    this.setState({price: event.target.value});
+  }
+
+  handleChangeEtatAvancement(event) {
+    this.setState({etat_avancement: event.target.options[event.target.selectedIndex].text});
+  }
+
+  handleChangeDateDebut(event) {
+    this.setState({date_debut: event.target.value});
+  }
+
+  handleChangeDateFin(event) {
+    this.setState({date_fin: event.target.value});
+  }
+
+  handleChangeChefId(event) {
+    this.setState({chef_id: event.target.options[event.target.selectedIndex].value});
+  }
+
+  handleSubmit(event) {
+      console.log('chef : ' + this.state.chef_id);
+    event.preventDefault();
+  }
+
+      componentDidMount = () => {
+        getIntervention(this.props.match.params.id).then(response => {
+            this.setState({
+                dataIntervention: response.data
+            });
+            this.setState({
+              signalisation_id: this.state.dataIntervention['signalisation_id']
+            });
+            this.setState({
+              price: this.state.dataIntervention['price']
+            });
+            this.setState({
+              etat_avancement: this.state.dataIntervention['etat_avancement']
+            });
+            this.setState({
+              date_debut: this.state.dataIntervention['date_debut']
+            });
+            this.setState({
+              date_fin: this.state.dataIntervention['date_fin']
+            });
+        });
+        all_signalisations().then(response => {
+            this.setState({
+                select_signalisation: response.data
+            });
+        });
+        all_chef().then(response => {
+            this.setState({
+                allChef: response.data
+            });
+        });
+
+        evaluer(this.props.match.params.id).then(response => {
+            if(response.data != ""){
+              this.setState({
+                hasEvaluer: true
+              });
+              getChefByID(response.data.user_id).then(response => {
+                this.setState({
+                  name: response.data['name']
+                });
+              });
+            }
+        });
+
+      }
+
+render() {
+
+// handle button click of signin form
+const handleUpdate = () => {
+    axios.put('http://127.0.0.1:8000/api/auth/intervention/' + this.props.match.params.id, {
+        signalisation_id    : this.state.signalisation_id,
+        price    : this.state.price,
+        etat_avancement    : this.state.etat_avancement,
+        date_debut    : this.state.date_debut,
+        date_fin    : this.state.date_fin
+    }).then(function (response) {
+      // setter
+      //localStorage.setItem('token', response.data.access_token)
+      //localStorage.setItem('id', response.data.user.id)
+      //localStorage.setItem('name', response.data.user.name)
+      //localStorage.setItem('email', response.data.user.email)
+      //localStorage.setItem('role', response.data.user.role)
+      // route for profile
+      console.log(response)
+    }).catch(function (error) {
+        console.log(error);
+    });
+}
+
+// handle button click of signin form
+const handleEvaluer  = () => {
+    axios.post('http://127.0.0.1:8000/api/auth/evaluer', {
+        user_id	: this.state.chef_id,
+        intervention_id : this.props.match.params.id
+    }).then(function (response) {
+      // setter
+      //localStorage.setItem('token', response.data.access_token)
+      //localStorage.setItem('id', response.data.user.id)
+      //localStorage.setItem('name', response.data.user.name)
+      //localStorage.setItem('email', response.data.user.email)
+      //localStorage.setItem('role', response.data.user.role)
+      // route for profile
+      console.log(response)
+    }).catch(function (error) {
+        console.log(error);
+    });
+}
+
+const delete_comment = () => {
+  axios.delete('http://127.0.0.1:8000/api/auth/comment/' + this.props.match.params.id)
+  .then(function (response) {
+    // setter
+    //const token = localStorage.setItem('token', response.data.access_token)
+    //const user = localStorage.setItem('user', response.data.user)
+    // route for profile
+    console.log(response)
+
+  }).catch(function (error) {
+    console.log('ibrahim => ' + error);
+  });
+}
+
+const fetch_signalisation = this.state.select_signalisation.map((element) =>
+<option value={element['id']}>{element['desc']}</option>
+);
+
+const fetchChef = this.state.allChef.map((element) =>
+<option value={element['id']}>{element['name']}</option>
+);
+
+return (<div className="container mt-5">
+    <Nav name="update comment" />
+    <div className="row">
+
+    <div className="col-md-12">
+            <div className="card border-0 shadow">
+                <div className="card-header border-0">Update Comment</div>
+
+                <div className="card-body">
+                    <form method="POST" onSubmit={this.handleSubmit}>
+
+                        <div className="form-group row">
+                            <label for="name" className="col-md-4 col-form-label text-md-right">Signalisations</label>
+                            <div className="col-md-8">
+                            <select class="custom-select custom-select-sm" name="signalisation_id" value={this.state.signalisation_id} onChange={this.handleChangeSignalisation_id}>
+                            {fetch_signalisation}
+                            </select>
+                            </div>
+                        </div>
+
+<div className="form-group row">
+    <label for="price" className="col-md-4 col-form-label text-md-right">Price</label>
+    <div className="col-md-8">
+        <input id="price" type="range" value={this.state.price} onChange={this.handleChangePrice} className="custom-range" min="0" max="150" name="price" required/>
+    </div>
+</div>
+
+<div className="form-group row">
+    <label for="etat_avancement" className="col-md-4 col-form-label text-md-right">Etat avancement</label>
+    <div className="col-md-8">
+    <select class="custom-select custom-select-sm" name="etat_avancement" value={this.state.etat_avancement} onChange={this.handleChangeEtatAvancement}>
+        <option value="debut">Debut</option>
+        <option value="moyenn">Moyenn</option>
+        <option value="avencer">avencer</option>
+        <option value="terminer">Terminer</option>
+    </select>
+    </div>
+</div>
+
+<div className="form-group row">
+    <label for="date" className="col-md-4 col-form-label text-md-right">Date Debut</label>
+    <div className="col-md-8">
+        <input id="date" type="date" value={this.state.date_debut} onChange={this.handleChangeDateDebut} className="form-control" name="date" required/>
+    </div>
+</div>
+
+<div className="form-group row">
+    <label for="date" className="col-md-4 col-form-label text-md-right">Date Fin</label>
+    <div className="col-md-8">
+        <input id="date" type="date" value={this.state.date_fin} onChange={this.handleChangeDateFin} className="form-control" name="date" required/>
+    </div>
+</div>
+
+                        <div className="form-group row mb-0">
+                            <div className="col-md-6 offset-md-4">
+                                <button type="submit" className="btn btn-outline-info" onClick={handleUpdate}>
+                                Update Intervention
+                                </button>
+                            </div>
+                            <button type="button" className="btn btn-outline-danger" onClick={delete_comment}>Delete</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    
+    </div>
+</div>
+);
+}
+
+}
