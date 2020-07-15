86c90df68af21bfc1bdd256cc8901e6310368b1c
ibrahimBougaoua <ibrahim.bougaoua@etu.usthb.dz>
2020-07-15 04:17:45 -0700

    Refactoring : add/update some funtions

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,4 +1,5 @@
 import React, { Component} from "react";
+import { Bar,Pie,Line,HorizontalBar } from 'react-chartjs-2';
 import axios from 'axios';
 import Nav from './nav';
 
@@ -25,6 +26,16 @@ async function equipeCount() {
 async function signalerCount(user_id) {
     try {
       const response = await axios.get('http://127.0.0.1:8000/api/auth/signalercount/' + user_id)
+      console.log(response);
+      return response;
+    } catch (error) {
+      console.error(error);
+    }
+}
+
+async function commentsCountDashboard(user_id) {
+    try {
+      const response = await axios.get('http://127.0.0.1:8000/api/auth/commentscountdashboard/' + user_id)
       console.log(response);
       return response;
     } catch (error) {
@@ -82,11 +93,57 @@ async function equipeDashboard() {
     }
 }
 
+async function userProfCountByRole() {
+    try {
+      const response = await axios.get('http://127.0.0.1:8000/api/auth/usercountbyrole/prof')
+      console.log(response);
+      return response;
+    } catch (error) {
+      console.error(error);
+    }
+}
+
+async function userEtudiantCountByRole() {
+    try {
+      const response = await axios.get('http://127.0.0.1:8000/api/auth/usercountbyrole/etudiant')
+      console.log(response);
+      return response;
+    } catch (error) {
+      console.error(error);
+    }
+}
+
+async function signalisationCommentsDashboard() {
+    try {
+      const response = await axios.get('http://127.0.0.1:8000/api/auth/signalisationcommentsdashboard')
+      console.log(response);
+      return response;
+    } catch (error) {
+      console.error(error);
+    }
+}
+
+async function signalisationEtatAvancementDashboard() {
+    try {
+      const response = await axios.get('http://127.0.0.1:8000/api/auth/signalisationetatavancementdashboard')
+      console.log(response);
+      return response;
+    } catch (error) {
+      console.error(error);
+    }
+}
+
+function getRole() {
+    if(localStorage.getItem('role'))
+    return localStorage.getItem('role');
+    return '';
+}
+
 export default class Dashboard extends Component {
     
     constructor(props) {
         super(props);
-        this.state = {all: [],usersCountRec: 0,equipeCountRec: 0,signalerCountRec: 0,signalisationCountRec: 0,signalisationDashboardRec: 0,userSignalisationDashboardRec: 0,userRoleDashboardRec: 0,equipedashboardRec: 0}
+        this.state = {labelsBar: [],dataBar: [],all: [],usersCountRec: 0,equipeCountRec: 0,signalerCountRec: 0,signalisationCountRec: 0,signalisationDashboardRec: 0,userSignalisationDashboardRec: 0,userRoleDashboardRec: 0,equipedashboardRec: 0,commentsCountRec: 0,etudiantCountRec: 0,profCountRec: 0,signalisationCommentsDashboardRec: 0,signalisationEtatAvancementDashboardRec: 0}
       }
 
       componentDidMount = () => {
@@ -103,6 +160,21 @@ export default class Dashboard extends Component {
         signalerCount(localStorage.getItem('id')).then(response => {
             this.setState({
                 signalerCountRec: response.data
+            });
+        });
+        commentsCountDashboard(localStorage.getItem('id')).then(response => {
+            this.setState({
+                commentsCountRec: response.data
+            });
+        });
+        userProfCountByRole().then(response => {
+            this.setState({
+                profCountRec: response.data
+            });
+        });
+        userEtudiantCountByRole().then(response => {
+            this.setState({
+                etudiantCountRec: response.data
             });
         });
         signalisationCount().then(response => {
@@ -130,49 +202,224 @@ export default class Dashboard extends Component {
                 equipedashboardRec: response.data
             });
         });
+        signalisationCommentsDashboard().then(response => {
+            this.setState({
+                signalisationCommentsDashboardRec: response.data
+            });
+        });
+        signalisationEtatAvancementDashboard().then(response => {
+            this.setState({
+                signalisationEtatAvancementDashboardRec: response.data
+            });
+        });
       }
       
 render() {
 
-console.log(this.state.signalisationDashboardRec)
+//console.log(this.state.signalisationDashboardRec)
 
 //this.state.signalisationDashboardRec.map((element,key) =>
 //<p key={key}>{element}</p>
 //);
 
+//this.state.signalisationDashboardRec.forEach(value => { console.log(value) });
+//console.log(Object.keys(this.state.signalisationDashboardRec).map(function(key) {return key;}));
+//console.log(Object.values(this.state.signalisationDashboardRec).map(function(value) {return value;}));
+   
+const bar = {
+    labels: Object.keys(this.state.signalisationDashboardRec).map(function(key) {return key;}),
+    datasets: [
+      {
+        label: Object.keys(this.state.signalisationDashboardRec).map(function(key) {return key;}),
+        data: Object.values(this.state.signalisationDashboardRec).map(function(value) {return value;}),
+        fill: false,          // Don't fill area under the line
+        borderColor: 'black'  // Line color
+      }
+    ]
+  }
+
+  const pie = {
+      labels: Object.keys(this.state.signalisationCommentsDashboardRec).map(function(key) {return key;}),
+      datasets: [
+        {
+          label: Object.keys(this.state.signalisationCommentsDashboardRec).map(function(key) {return key;}),
+          data: Object.values(this.state.signalisationCommentsDashboardRec).map(function(value) {return value;}),
+          fill: false,          // Don't fill area under the line
+          borderColor: 'black'  // Line color
+        }
+      ]
+    }
+
+    const line = {
+        labels: Object.keys(this.state.signalisationEtatAvancementDashboardRec).map(function(key) {return key;}),
+        datasets: [
+          {
+            label: Object.keys(this.state.signalisationEtatAvancementDashboardRec).map(function(key) {return key;}),
+            data: Object.values(this.state.signalisationEtatAvancementDashboardRec).map(function(value) {return value;}),
+            fill: false,          // Don't fill area under the line
+            borderColor: 'black'  // Line color
+          }
+        ]
+      }
+    
 return (
 <div className="container mt-5">
     <Nav name="Dashboard" />
 
     <div className="row">
-        <div className="col-md-3">
-        <div class="shadow p-3 mb-5 bg-white rounded">
-            <button type="button" class="btn btn-info">
-                Number of users <span class="badge badge-light">{this.state.usersCountRec}</span>
-            </button>
+        
+        { getRole() == 'aaa' || getRole() == 'aaa'
+          ? <div className="col-xl-4 col-md-6 mb-4">
+          <div className="card border-left-primary shadow h-100 py-2 bg-light border-0">
+            <div className="card-body">
+              <div className="row no-gutters align-items-center">
+                <div className="col mr-2">
+                  <div className="text-xs font-weight-bold text-info text-uppercase mb-1">Number of users</div>
+                  <div className="h5 mb-0 font-weight-bold text-gray-800">{this.state.usersCountRec}</div>
+                </div>
+                <div className="col-auto">
+                  <i className="fas fa-user fa-2x text-muted"></i>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
+        : null
+        }
+
+        { getRole() == 'aaa' || getRole() == 'aaa'
+          ? <div className="col-xl-4 col-md-6 mb-4">
+          <div className="card border-left-primary shadow h-100 py-2 bg-white border-0">
+            <div className="card-body">
+              <div className="row no-gutters align-items-center">
+                <div className="col mr-2">
+                  <div className="text-xs font-weight-bold text-info text-uppercase mb-1">Number of equipes</div>
+                  <div className="h5 mb-0 font-weight-bold text-gray-800">{this.state.equipeCountRec}</div>
+                </div>
+                <div className="col-auto">
+                  <i className="fas fa-user fa-2x text-muted"></i>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="col-md-3">
-        <div class="shadow p-3 mb-5 bg-white rounded">
-            <button type="button" class="btn btn-info">
-                Number of equipes <span class="badge badge-light">{this.state.equipeCountRec}</span>
-            </button>
+        : null
+        }
+
+        { getRole() == 'prof' || getRole() == 'etudiant'
+          ? <div className="col-md-3 mb-4">
+  <div className="card border-left-primary shadow h-100 py-2 bg-white border-0">
+    <div className="card-body">
+      <div className="row no-gutters align-items-center">
+        <div className="col mr-2">
+          <div className="text-sm font-weight-bold text-info text-uppercase mb-1">signale</div>
+          <div className="h5 mb-0 font-weight-bold text-gray-800">{this.state.signalerCountRec}</div>
         </div>
+        <div className="col-auto">
+          <i className="fas fa-user fa-2x text-muted"></i>
         </div>
-        <div className="col-md-3">
-        <div class="shadow p-3 mb-5 bg-white rounded">
-            <button type="button" class="btn btn-info">
-                Number of signale <span class="badge badge-light">{this.state.signalerCountRec}</span>
-            </button>
+      </div>
+    </div>
+  </div>
+</div>
+        : null
+        }
+
+        { getRole() == 'prof' || getRole() == 'etudiant'
+          ? <div className="col-md-3 mb-4">
+  <div className="card border-left-primary shadow h-100 py-2 bg-white border-0">
+    <div className="card-body">
+      <div className="row no-gutters align-items-center">
+        <div className="col mr-2">
+          <div className="text-xs font-weight-bold text-info text-uppercase mb-1">Etudiants</div>
+          <div className="h5 mb-0 font-weight-bold text-gray-800">{this.state.etudiantCountRec}</div>
         </div>
+        <div className="col-auto">
+          <i className="fas fa-user fa-2x text-muted"></i>
         </div>
-        <div className="col-md-3">
-        <div class="shadow p-3 mb-5 bg-white rounded">
-            <button type="button" class="btn btn-info">
-                Number of signalisation <span class="badge badge-light">{this.state.signalisationCountRec}</span>
-            </button>
+      </div>
+    </div>
+  </div>
+</div>
+        : null
+        }
+
+        { getRole() == 'prof' || getRole() == 'etudiant'
+          ? <div className="col-md-3 mb-4">
+          <div className="card border-left-primary shadow h-100 py-2 bg-white border-0">
+            <div className="card-body">
+              <div className="row no-gutters align-items-center">
+                <div className="col mr-2">
+                  <div className="text-xs font-weight-bold text-info text-uppercase mb-1">Profs</div>
+                  <div className="h5 mb-0 font-weight-bold text-gray-800">{this.state.profCountRec}</div>
+                </div>
+                <div className="col-auto">
+                  <i className="fas fa-user fa-2x text-muted"></i>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
+        : null
+        }
+
+        
+        { getRole() == 'prof' || getRole() == 'etudiant'
+          ? <div className="col-md-3 mb-4">
+          <div className="card border-left-primary shadow h-100 py-2 bg-white border-0">
+            <div className="card-body">
+              <div className="row no-gutters align-items-center">
+                <div className="col mr-2">
+                  <div className="text-xs font-weight-bold text-info text-uppercase mb-1">Comments</div>
+                  <div className="h5 mb-0 font-weight-bold text-gray-800">{this.state.commentsCountRec}</div>
+                </div>
+                <div className="col-auto">
+                  <i className="fas fa-user fa-2x text-muted"></i>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
+        : null
+        }
+
+        { getRole() == 'aaa' || getRole() == 'aaaa'
+          ? <div className="col-xl-4 col-md-6 mb-4">
+          <div className="card border-left-primary shadow h-100 py-2 bg-white border-0">
+            <div className="card-body">
+              <div className="row no-gutters align-items-center">
+                <div className="col mr-2">
+                  <div className="text-xs font-weight-bold text-info text-uppercase mb-1">Number of signalisation</div>
+                  <div className="h5 mb-0 font-weight-bold text-gray-800">{this.state.signalisationCountRec}</div>
+                </div>
+                <div className="col-auto">
+                  <i className="fas fa-user fa-2x text-muted"></i>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        : null
+        }
+
+      <div className="col-md-6">
+        <div className="card border-left-primary shadow h-100 py-2 bg-white border-0">
+            <Bar data={bar} />
+        </div>
+      </div>
+      
+      <div className="col-md-6">
+        <div className="card border-left-primary shadow h-100 py-2 bg-white border-0">
+            <Pie data={pie} />
+        </div>
+      </div>
+
+      <div className="col-md-12 mt-3">
+        <div className="card border-left-primary shadow h-100 py-2 bg-white border-0">
+            <Line data={line} />
+        </div>
+      </div>
+
     </div>
 
 </div>
