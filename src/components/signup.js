7053110ef367243dd319d00c890c9bed7752028b
ibrahimBougaoua <ibrahim.bougaoua@etu.usthb.dz>
2020-08-29 08:21:49 -0700

    Refactoring : register

--- src/components/signup.js
+++ src/components/signup.js
@@ -63,6 +63,8 @@ export default class Signup extends Component {
 
     render() {
 
+const { history } = this.props;
+
 // handle button click of signin form
 const handleSignin = () => {
     axios.post('http://127.0.0.1:8000/api/auth/register', {
@@ -74,12 +76,14 @@ const handleSignin = () => {
         role    : this.state.role,
     }).then(function (response) {
       // setter
-      //localStorage.setItem('token', response.data.access_token)
-      //localStorage.setItem('id', response.data.user.id)
-      //localStorage.setItem('name', response.data.user.name)
-      // localStorage.setItem('email', response.data.user.email)
-      //localStorage.setItem('role', response.data.user.role)
+      localStorage.setItem('token', response.data.access_token)
+      localStorage.setItem('id', response.data.user.id)
+      localStorage.setItem('name', response.data.user.name)
+      localStorage.setItem('email', response.data.user.email)
+      localStorage.setItem('role', response.data.user.role)
       // route for profile
+      history.push('/profile');
+
       console.log(response)
     }).catch(function (error) {
         console.log(error);
