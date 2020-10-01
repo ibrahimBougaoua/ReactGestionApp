ff6f2671a8a01a0489b4e4f4533f5810049210e2
ibrahimBougaoua <ibrahim.bougaoua@etu.usthb.dz>
2020-10-01 13:03:09 -0700

    Refactoring : view

--- src/components/view.js
+++ src/components/view.js
@@ -57,12 +57,45 @@ async function all_signalisations() {
     }
 }
 
-function hideShowSubComment(id) {
-  window.$(function() {
-    window.$('.click_' + id).click(function() {
-      window.$('.reply_' + id).toggle();
-    })
-  })
+function handleCreate(user_id,reply_id,signalisation_id,name,email,message)
+{
+  axios.post('http://127.0.0.1:8000/api/auth/comment', {
+      user_id : user_id,
+      reply_id : reply_id,
+      signalisation_id : signalisation_id,
+      name : name,
+      mail : email,
+      comment : message
+  }).then(function (response) {
+    // setter
+    // localStorage.setItem('token', response.data.access_token)
+    // localStorage.setItem('name', response.data.user.name)
+    // localStorage.setItem('email', response.data.user.email)
+    // localStorage.setItem('role', response.data.user.role)
+    // route for profile
+    console.log(response)
+  }).catch(function (error) {
+      console.log(error);
+  });
+}
+
+function handleSignaler(user_id,signalisation_id)
+{
+  axios.post('http://127.0.0.1:8000/api/auth/signaler', {
+      user_id    : user_id,
+      signalisation_id    : signalisation_id,
+  }).then(function (response) {	
+    // setter
+    //localStorage.setItem('token', response.data.access_token)
+    //localStorage.setItem('id', response.data.user.id)
+    //localStorage.setItem('name', response.data.user.name)
+    //localStorage.setItem('email', response.data.user.email)
+    //localStorage.setItem('role', response.data.user.role)
+    // route for profile
+    console.log(response)
+  }).catch(function (error) {
+      console.log(error);
+  });
 }
 
 export default class View extends Component {
@@ -119,7 +152,7 @@ constructor(props) {
         });
         hasSignaler(localStorage.getItem('id')).then(response => {
             console.log(response.data);
-            if(this.props.match.params.id == response.data.signalisation_id)
+            if(response.data != null)
             {
                 this.setState({
                     hasSn: true
@@ -130,101 +163,12 @@ constructor(props) {
 
 render() {
 
-console.log(this.state.hasSn);
-
-// handle button click of signin form
-const handleCreate = (replyId) => {
-  axios.post('http://127.0.0.1:8000/api/auth/comment', {
-      user_id : localStorage.getItem('id'),
-      reply_id : replyId,
-      signalisation_id : this.props.match.params.id,
-      name : this.state.name,
-      mail : this.state.email,
-      comment : this.state.message
-  }).then(function (response) {
-    // setter
-    // localStorage.setItem('token', response.data.access_token)
-    // localStorage.setItem('name', response.data.user.name)
-    // localStorage.setItem('email', response.data.user.email)
-    // localStorage.setItem('role', response.data.user.role)
-    // route for profile
-    console.log(response)
-  }).catch(function (error) {
-      console.log(error);
-  });
-}
-
-// handle button click of signin form
-const handleSignaler = () => {
-    axios.post('http://127.0.0.1:8000/api/auth/signaler', {
-        user_id    : localStorage.getItem('id'),
-        signalisation_id    : this.props.match.params.id,
-    }).then(function (response) {	
-      // setter
-      //localStorage.setItem('token', response.data.access_token)
-      //localStorage.setItem('id', response.data.user.id)
-      //localStorage.setItem('name', response.data.user.name)
-      //localStorage.setItem('email', response.data.user.email)
-      //localStorage.setItem('role', response.data.user.role)
-      // route for profile
-      console.log(response)
-    }).catch(function (error) {
-        console.log(error);
-    });
-}
-
-
 const all_comments = this.state.allComment.map((element) =>
 <div className="card border-0 shadow mb-3">
 <div className="card-body">
-<p className="text-muted">{element.comment}</p>
-<small className="text-sm mr-2">{element.name}()</small>
-<small className="text-sm">{element.created_at}</small>
-<input type="hidden" name={'reply_' + element.id} value={element.id}/>
-<button className={"btn btn-sm btn-outline-info float-right click_" + element.id} onClick={hideShowSubComment(element.id)}>Replay</button>
-<div className={'mt-4 reply_' + element.id} style={{display:'none'}}>
-  
-<div className="card border-0 shadow">
-                <div className="card-body">
-                    <form method="POST" onSubmit={this.handleSubmit}>
-                    <div class="form-row">
-                        <div className="form-group col-md-6">
-                            <label for="name">Name</label>
-                            <input id="name" type="text" value={this.state.name} onChange={this.handleChangeName} className={ this.state.name == '' && this.state.vd ? 'form-control is-invalid' : "form-control is-valid" } name="name" placeholder="name" required/>
-                            { this.state.name == '' && this.state.vd
-                              ? <div className="invalid-feedback"> This field is empty.</div>
-                              : null
-                            }
-                        </div>
-
-                        <div className="form-group col-md-6">
-                            <label for="email">E-Mail Address</label>
-                            <input id="email" type="email" value={this.state.email} onChange={this.handleChangeEmail} className={ this.state.email == '' && this.state.vd ? 'form-control is-invalid' : "form-control is-valid" } name="email" placeholder="exmple@mail.dz" required/>
-                            { this.state.email == '' && this.state.vd
-                              ? <div className="invalid-feedback"> This field is empty.</div>
-                              : null
-                            }
-                        </div>
-
-                        <div className="form-group col-md-12">
-                            <label for="telephone">Message</label>
-                            <textarea id="exampleFormControlTextarea1" rows="3" name="message" onChange={this.handleChangeMessage} className={ this.state.message == '' && this.state.vd ? 'form-control is-invalid' : "form-control is-valid" } >{this.state.message}</textarea>
-                        </div>
-
-                        <div className="form-group col-md-12 mb-0">
-                            <button type="submit" className="btn btn-outline-info" onClick={handleCreate(element.id)} >Comment</button>
-                        </div>
-                      </div>
-                    </form>
-                </div>
-            </div> 
-
-
-
-
-
-
-</div>
+<p className="card-text text-sm text-muted">Commentaire : {element.comment}</p>
+<p className="card-text text-sm text-muted">Auteur(e) : {element.name}</p>
+<p className="card-text text-sm text-muted">Créé le : {Moment(element.created_at).format('DD-MM-YYYY')}</p>  
 </div>
 </div>
 );
@@ -242,24 +186,26 @@ return (<div className="container-fluid mt-5">
     </div>
     <div className="col-md-8">
       <div className="card-body">
-        <p className="card-text">Description : {this.state.dataEquipe['desc']}</p>
+        <p className="card-text text-muted">Description : {this.state.dataEquipe['desc']}</p>
         
-    <div style={{width:'100%',height:'50%'}}>
-    <p className="card-text">Localisation :</p>
+<div style={{width:'100%',height:'50%'}}>
+<p className="card-text text-muted">Localisation :</p>
 <MyMapComponent
   isMarkerShown
-  googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+  googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyAv9RaNVTKPm_A3VLYUJHa9i-pnzi7TZAg
+  "
   loadingElement={<div style={{ height: `100%` }} />}
   containerElement={<div style={{ height: `200px` }} />}
   mapElement={<div style={{ height: `100%` }} />}
 />
 </div>
         
-        <p className="card-text mt-2">Lieu : {this.state.dataEquipe['lieu']}</p>
-        <p className="card-text">Nature : {this.state.dataEquipe['nature']}</p>
-        <p className="card-text">Cause : {this.state.dataEquipe['cause']}</p>
-        <p className="card-text">Auteur(e) : {this.state.dataEquipe['user_name']}</p>
-        <p className="card-text"><small className="text-muted">Créé le : {Moment(this.state.dataEquipe['created_at']).format('DD-MM-YYYY')}</small></p>
+        <p className="card-text mt-3 text-muted">Lieu : {this.state.dataEquipe['lieu']}</p>
+        <p className="card-text text-muted">Nature : {this.state.dataEquipe['nature']}</p>
+        <p className="card-text text-muted">Cause : {this.state.dataEquipe['cause']}</p>
+        <p className="card-text text-muted">Auteur(e) : {this.state.dataEquipe['user_name']}</p>
+        <p className="card-text text-muted"><small className="text-muted">Créé le : {Moment(this.state.dataEquipe['created_at']).format('DD-MM-YYYY')}</small></p>
+        {this.state.hasSn ? <button type="submit" className="btn btn-info float-right" style={{position:"relative",bottom:"20px"}} onClick={() => {handleSignaler(localStorage.getItem('id'),this.props.match.params.id)}}>Signaler</button> : null}
       </div>
     </div>
   </div>
@@ -273,7 +219,7 @@ return (<div className="container-fluid mt-5">
                     <form method="POST" onSubmit={this.handleSubmit}>
                     <div class="form-row">
                         <div className="form-group col-md-6">
-                            <label for="name">Name</label>
+                            <label for="name">Nom</label>
                             <input id="name" type="text" value={this.state.name} onChange={this.handleChangeName} className={ this.state.name == '' && this.state.vd ? 'form-control is-invalid' : "form-control is-valid" } name="name" placeholder="name" required/>
                             { this.state.name == '' && this.state.vd
                               ? <div className="invalid-feedback"> This field is empty.</div>
@@ -282,7 +228,7 @@ return (<div className="container-fluid mt-5">
                         </div>
 
                         <div className="form-group col-md-6">
-                            <label for="email">E-Mail Address</label>
+                            <label for="email">E-mail</label>
                             <input id="email" type="email" value={this.state.email} onChange={this.handleChangeEmail} className={ this.state.email == '' && this.state.vd ? 'form-control is-invalid' : "form-control is-valid" } name="email" placeholder="exmple@mail.dz" required/>
                             { this.state.email == '' && this.state.vd
                               ? <div className="invalid-feedback"> This field is empty.</div>
@@ -291,12 +237,12 @@ return (<div className="container-fluid mt-5">
                         </div>
 
                         <div className="form-group col-md-12">
-                            <label for="telephone">Message</label>
+                            <label for="telephone">Commentaire</label>
                             <textarea id="exampleFormControlTextarea1" rows="3" name="message" onChange={this.handleChangeMessage} className={ this.state.message == '' && this.state.vd ? 'form-control is-invalid' : "form-control is-valid" } >{this.state.message}</textarea>
                         </div>
 
                         <div className="form-group col-md-12 mb-0">
-                            <button type="submit" className="btn btn-outline-info" onClick={handleCreate(0)} >Comment</button>
+                            <button type="submit" className="btn btn-outline-info" onClick={() => {handleCreate(localStorage.getItem('id'),0,this.props.match.params.id,this.state.name,this.state.email,this.state.message)}} >Créé</button>
                         </div>
                       </div>
                     </form>
